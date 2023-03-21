5950189771b57dc4534202cbbefd5846c27faec9
Habeebah157 <63429228+Habeebah157@users.noreply.github.com>
2023-03-20 21:34:35 -0400

    message

---
+++ app/src/main/java/com/codepath/articlesearch/Article.kt
@@ -0,0 +1,27 @@
+package com.example.flixter2
+import android.support.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Keep
+@Serializable
+data class BaseResponse(
+    @SerialName("results")
+    val docs: List<Movies>?
+)
+@Keep
+@Serializable
+data class Movies(
+    @SerialName("poster_path")
+    val poster: String?,
+    @SerialName("adult")
+    val rating: Boolean?,
+    @SerialName("overview")
+    val overview: String?,
+    @SerialName("original_title")
+    val Original: String?,
+): java.io.Serializable{
+    val mediaImageUrl = "https://image.tmdb.org/t/p/w500${poster}"
+}
+
---
+++ app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
@@ -0,0 +1,44 @@
+package com.codepath.articlesearch
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.flixter2.Movies
+
+private const val TAG = "DetailActivity"
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var mediaImageView: ImageView
+    private lateinit var titleTextView: TextView
+    private lateinit var bylineTextView: TextView
+    private lateinit var abstractTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        // TODO: Find the views for the screen
+        mediaImageView = findViewById(R.id.mediaImage)
+        titleTextView = findViewById(R.id.mediaTitle)
+        bylineTextView = findViewById(R.id.mediaByline)
+        abstractTextView = findViewById(R.id.mediaAbstract)
+
+        // TODO: Get the extra from the Intent
+        val movies = intent.getSerializableExtra(ARTICLE_EXTRA) as Movies
+
+
+
+
+        // TODO: Set the title, byline, and abstract information from the article
+        titleTextView.text = movies.Original
+        bylineTextView.text = movies.overview
+        abstractTextView.text= movies.rating.toString()
+        Glide.with(this)
+            .load(movies.mediaImageUrl)
+            .into(mediaImageView)
+
+        // TODO: Load the media image
+    }
+}
